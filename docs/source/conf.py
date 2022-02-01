e8023b01d8c43c4446a0481809962427aca64c3f
aleksanm <59065138+aleksanm@users.noreply.github.com>
2022-02-01 16:58:03 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'laravel-mix-django'
+copyright = '2022, Alex'
+author = 'Alex'
 
 release = '0.1'
 version = '0.1.0'
